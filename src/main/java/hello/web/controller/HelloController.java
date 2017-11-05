95c10df3f87fdadb3ab96ae9535a0394f933050d
tab <tabchanj@gmail.com>
2017-11-05 22:41:12 +0800

    website II

--- src/main/java/hello/web/controller/HelloController.java
+++ src/main/java/hello/web/controller/HelloController.java
@@ -5,10 +5,11 @@ import hello.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
     @Autowired
@@ -20,5 +21,9 @@ public class HelloController {
     @GetMapping("/hello")
     public Object sayHello(){
         return userService.findOne(5);
+    }
+    @GetMapping("/hellopage")
+    public String Hello(){
+        return "hello";
     }
 }
